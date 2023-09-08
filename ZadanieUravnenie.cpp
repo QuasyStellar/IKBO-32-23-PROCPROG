b458b1b06f33c6b7ec9ab70a6c310ce41efc5a3c
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-08 16:39:55 +0300

    Update ZadanieUravnenie.cpp

--- ZadanieUravnenie.cpp
+++ ZadanieUravnenie.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <string>
 
 
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
+//определение переменных
     float b, c;
+//запрос ввода пользователя
+    cout << "Введите числа b и c в формате 'b c' для решения уравнения bx+c = 0: ";
     cin >> b >> c;
+//проверка Исключающая деление на 0
     if (b != 0) {
-        cout << -c / b << endl;
+//проВерка для предотврАщения вывода -0
+        if (c == 0) { cout << "Для b = " << b << " и c = " << c << " x = " << 0 << endl; }
+        else {
+            cout << "Для b = " << b << " и c = " << c << " x = " << -c / b << endl; }
     }
+//проверка возможНых исходов при b=0
     else if (c != 0) {
-        cout << "Null" << endl;
+        cout << "Для b = " << b << " и c = " << c << " корней нет" << endl;
     }
     else {
-        cout << "A" << endl;
+        cout << "Для b = " << b << " и c = " << c << " x - любое число" << endl;
     }
 }
