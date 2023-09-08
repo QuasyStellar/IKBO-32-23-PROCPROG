ea6b446c67a08c858a1e44c706cd4282a15299e1
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-08 16:15:08 +0300

    Update ZadanieArifmetika.cpp

--- ZadanieArifmetika.cpp
+++ ZadanieArifmetika.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
+#include <string>
 
 
 using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     float a, b;
+    cout << "Введите 2 числа с клавиатуры в формате 'a b': ";
     cin >> a >> b;
+    cout << "Сложение" << endl;
     cout << a + b << endl;
+    cout << "Вычитание" << endl;
     cout << a - b << endl;
+    cout << "Умножение" << endl;
     cout << a * b << endl;
-    cout << a / b << endl;
+    cout << "Деление" << endl;
+    if (b !=0){cout << a / b << endl;}
+    else{cout <<"На ноль не делим" << endl;}
+    
 }
