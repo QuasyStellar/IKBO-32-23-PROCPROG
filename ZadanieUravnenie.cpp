dd1ca9a43418ece5927411ab730e8c2a11b0da1c
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-08 11:20:12 +0300

    Update ZadanieUravnenie.cpp

--- ZadanieUravnenie.cpp
+++ ZadanieUravnenie.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main()
 {
-    int b, c;
+    float b, c;
     cin >> b >> c;
     if (b != 0) {
         cout << -c / b << endl;
     }
     else if (c != 0) {
-        cout << 0 << endl;
+        cout << "Null" << endl;
     }
     else {
         cout << "A" << endl;
