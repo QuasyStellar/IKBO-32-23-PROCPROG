051274a473f1a85f5f2433969322546879feeef9
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-16 18:10:47 +0300

    Update ZadanieTabulation.cpp

--- WEEK2/ZadanieTabulation.cpp
+++ WEEK2/ZadanieTabulation.cpp
@@ -10,8 +10,8 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	float x = -4.0;		//определение переменых
 	cout << "x  |  y"<< endl;
-	while (x <= 4) {
-		if ((x - 1) != 0) {
+	while (x <= 4) { // табуляция функции
+		if ((x - 1) != 0) { // исключаем деление на ноль
 			cout << x << " | " <<(pow(x, 2) - 2 * x + 2) / (x - 1) << endl;
 		}
 		else{
