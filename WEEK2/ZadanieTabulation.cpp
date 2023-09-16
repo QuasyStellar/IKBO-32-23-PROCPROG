c45426bdf97febdfedb83e32af9dbc4b457b16fe
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-16 17:36:05 +0300

    Create ZadanieTabulation.cpp

---
+++ WEEK2/ZadanieTabulation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x = -4.0;		//определение переменых
+	cout << "x  |  y"<< endl;
+	while (x <= 4) {
+		if ((x - 1) != 0) {
+			cout << x << " | " <<(pow(x, 2) - 2 * x + 2) / (x - 1) << endl;
+		}
+		x += 0.5;
+	}
+}
