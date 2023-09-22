7a5592cb85dbbe8d5cb201228c3e04921f7d9ac0
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-22 19:42:54 +0300

    Create ZadanieZaem.cpp

---
+++ WEEK3/ZadanieZaem.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float S, p, n;		//определение переменых
+	cout << "Введите сумму займа, процент и кол-во лет в формате 'S p n':";
+	cin >> S >> p >> n; //получаем введенные данные
+	if (S > 0, n > 0) {
+		cout << "Месячная выплата m =" << (S * (p / 100) * pow((1 + (p / 100)), n)) / (12 * (pow((1 + (p / 100)), n) - 1));
+	}
+	else {
+		cout << "Введены некорректные данные";
+	}
+}
