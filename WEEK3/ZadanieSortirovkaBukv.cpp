7861b18dea22e831071fc618fb0c7cda9b8a93ba
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-23 18:58:25 +0300

    Update ZadanieSortirovkaBukv.cpp

--- WEEK3/ZadanieSortirovkaBukv.cpp
+++ WEEK3/ZadanieSortirovkaBukv.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <algorithm>
 
 
 using namespace std;
@@ -10,24 +9,14 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 	string str;
-	bool flag = 1;
-	cout << "Введите строку из 30 букв: ";    //dfghrtyrjtlkejlkdfjglkjdfgjkhy пример строки
+	cout << "Введите строку: ";    //dfghrtyrjtlkejlkdfjglkjdfgjkhy пример строки
 	cin >> str;
-	if (str.size() == 30) {
-		for (int i = 0; i < str.size(); i++) {
-			if (!isalpha(str[i])) {
-				flag = 0;
+	for (int i = 0; i < str.size() - 1; i++) {
+		for (int j = str.size() - 1; j > i; j--) {
+			if (str[j] < str[j - 1]) {
+				swap(str[j], str[j - 1]);
 			}
 		}
-		if (flag) {
-			sort(str.begin(), str.end());
-			cout << str;
-		}
-		else{
-			cout << "В строке не только буквы";
-		}
 	}
-	else {
-		cout << "Длина строки не 30 символов";
-	}
+	cout << str;
 }
