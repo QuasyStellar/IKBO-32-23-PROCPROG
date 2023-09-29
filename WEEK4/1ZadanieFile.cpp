830a692a8bfc07ed161ca91e26a9519a381134e2
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-09-29 22:44:54 +0300

    Update 1ZadanieFile.cpp

--- WEEK4/1ZadanieFile.cpp
+++ WEEK4/1ZadanieFile.cpp
@@ -12,30 +12,35 @@ int main()
 	float str;
 	float sum, enter;
 	file.open("textfile.txt");
-	cout << "Введите числа для записи в файл: "<< endl
-		;
-	for (int i = 0; i < 10; i++) {
-		cin >> enter;
-		if (cin.fail()) {
-			cout << "Ошибка ввода. Было введено не число" << endl;
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-			i--;
-			continue;
-		}
-		file << enter << endl;
-	}
-	file.close();
-	ifstream files("textfile.txt");
-	if (files.is_open()) {
-		sum = 0;
-		while (files >> str) {
-			sum += str;
-		}
-		cout <<"Сумма чисел = " << sum << endl;
-		files.close();
+	if (!file.is_open())
+	{
+		cout << "Не удалось создать файл." << endl;
 	}
 	else {
-		cout << "Файл не был открыт из-за ошибки";
+		cout << "Введите числа для записи в файл: " << endl;
+		for (int i = 0; i < 10; i++) {
+			cin >> enter;
+			if (cin.fail()) {
+				cout << "Ошибка ввода. Было введено не число" << endl;
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				i--;
+				continue;
+			}
+			file << enter << endl;
+		}
+		file.close();
+		ifstream files("textfile.txt");
+		if (files.is_open()) {
+			sum = 0;
+			while (files >> str) {
+				sum += str;
+			}
+			cout << "Сумма чисел = " << sum << endl;
+			files.close();
+		}
+		else {
+			cout << "Файл не был открыт из-за ошибки";
+		}
 	}
 }
