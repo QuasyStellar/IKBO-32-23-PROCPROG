425060a8f211d46039ad70b2925932da8f74ab3f
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-10-07 11:41:38 +0300

    Create 6ZadanieAvtomat.cpp

---
+++ WEEK4/6ZadanieAvtomat.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int romanToNumber(char romanChar) {
+    switch (toupper(romanChar)) {
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    default:  return 0;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru-RU");
+    int decimal = 0, prevValue = 0, count = 1;
+    bool isValid = true, isVichet = false;
+    string input;
+
+    cout << "Введите римское число: ";
+    cin >> input;
+
+    for (int i = input.length() - 1; i >= 0; i--) {
+        int currentValue = romanToNumber(input[i]);
+
+        if (currentValue == 0) {
+            isValid = false;
+            break;
+        }
+
+        if (currentValue < prevValue) {
+            isVichet = true;
+            if (prevValue / currentValue != 10 && prevValue / currentValue != 5) {
+                isValid = false;
+                break;
+            }
+            if (count > 1) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (currentValue == prevValue) {
+            count++;
+            if (count > 3 or currentValue == 5 or isVichet) {
+                isValid = false;
+                break;
+            }
+        }
+        else {
+            count = 1;
+        }
+
+        if (currentValue < prevValue) {
+            decimal -= currentValue;
+        }
+        else {
+            decimal += currentValue;
+        }
+        prevValue = currentValue;
+    }
+
+    if (!isValid) {
+        cout << "Неверный формат римского числа." << endl;
+    }
+    else {
+        cout << "Результат перевода: " << decimal << endl;
+    }
+}
