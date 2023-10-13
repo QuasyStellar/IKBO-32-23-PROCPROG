91f89580c0ddf7dc886eff8eecd2963cf7517542
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-10-13 20:19:52 +0300

    Update 3ZadanieGeometricheskieFiguri.cpp

--- WEEK4/3ZadanieGeometricheskieFiguri.cpp
+++ WEEK4/3ZadanieGeometricheskieFiguri.cpp
@@ -7,8 +7,9 @@ float rectangle(float length, float width) {
     return length * width;
 }
 
-float triangle(float base, float height) {
-    return 0.5f * base * height;
+float triangle(float st1, float st2, float st3) {
+    float p = (st1 + st2 + st3) / 2.0f;
+    return sqrt(p*(p-st1)*(p-st2)*(p-st3));
 }
 
 float circle(float radius) {
@@ -18,7 +19,7 @@ float circle(float radius) {
 int main() {
     setlocale(LC_ALL, "Russian");
     int choice;
-    float area;
+    float area = 0;
     cout << "Выберите фигуру для вычисления площади:" << endl;
     cout << "1. Прямоугольник" << endl;
     cout << "2. Треугольник" << endl;
@@ -30,23 +31,40 @@ int main() {
         float length, width;
         cout << "Введите длину и ширину прямоугольника: ";
         cin >> length >> width;
-        area = rectangle(length, width);
+        if (length > 0 and width > 0) {
+            area = rectangle(length, width);
+        }
+        else {
+            cout << "Некорректные значения";
+        }
         break;
     case 2:
-        float base, height;
-        cout << "Введите основание и высоту треугольника: ";
-        cin >> base >> height;
-        area = triangle(base, height);
+        float st1, st2, st3;
+        cout << "Введите все стороны треугольника: ";
+        cin >> st1 >> st2 >> st3;
+        if (st1 > 0 and st2 > 0 and st3 > 0 and st1 + st2 > st3 or st1 + st3 > st2 or st2 + st3 > st1) {
+            area = triangle(st1, st2, st3);
+        }
+        else {
+            cout << "Некорректные значения";
+        }
         break;
     case 3:
         float radius;
         cout << "Введите радиус круга: ";
         cin >> radius;
-        area = circle(radius);
+        if (radius > 0) {
+            area = circle(radius);
+        }
+        else {
+            cout << "Некорректные значения";
+        }
         break;
     default:
         cout << "Неверный выбор." << endl;
         return 1;
     }
-    cout << "Площадь выбранной фигуры: " << area << endl;
+    if (area){
+    cout << "Площадь выбранной фигуры: " << area << endl; 
+    }
 }
