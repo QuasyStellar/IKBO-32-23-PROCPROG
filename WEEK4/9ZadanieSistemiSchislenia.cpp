7d9d295a2447b11f2ec7eadf3efa98f92a1dc54a
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-10-13 20:57:54 +0300

    Update 9ZadanieSistemiSchislenia.cpp

--- WEEK4/9ZadanieSistemiSchislenia.cpp
+++ WEEK4/9ZadanieSistemiSchislenia.cpp
@@ -4,60 +4,49 @@
 
 using namespace std;
 
-int charToInt(char c) {
-    switch (toupper(c)){
-    case '0':
-        return 0;
-        break;
-    case '1':
-        return 1;
-        break;
-    case '2':
-        return 2;
-        break;
-    case '3':
-        return 3;
-        break;
-    case '4':
-        return 4;
-        break;
-    case '5':
-        return 5;
-        break;
-    case '6':
-        return 6;
-        break;
-    case '7':
-        return 7;
-        break;
-    case '8':
-        return 8;
-        break;
-    case '9':
-        return 9;
-        break;
-    case 'A':
-        return 10;
-        break;
-    case 'B':
-        return 11;
-        break;
-    case 'C':
-        return 12;
-        break;
-    case 'D':
-        return 13;
-        break;
-    case 'E':
-        return 14;
-        break;
-    case 'F':
-        return 15;
-        break;
-    default:
-        return -1;
-        break;
-    }
+int charToInt(char symbol) {
+	switch (symbol){
+	case '0': return 0; break;
+	case '1': return 1; break;
+	case '2': return 2; break;
+	case '3': return 3; break;
+	case '4': return 4; break;
+	case '5': return 5; break;
+	case '6': return 6; break;
+	case '7': return 7; break;
+	case '8': return 8; break;
+	case '9': return 9; break;
+	case 'A': return 10; break;
+	case 'B': return 11; break;
+	case 'C': return 12; break;
+	case 'D': return 13; break;
+	case 'E': return 14; break;
+	case 'F': return 15; break;
+	}
+	return -1;
+}
+
+
+char intTochar(int symbol) {
+	switch (symbol) {
+	case 0: return '0'; break;
+	case 1: return '1'; break;
+	case 2: return '2'; break;
+	case 3: return '3'; break;
+	case 4: return '4'; break;
+	case 5: return '5'; break;
+	case 6: return '6'; break;
+	case 7: return '7'; break;
+	case 8: return '8'; break;
+	case 9: return '9'; break;
+	case 10: return 'A'; break;
+	case 11: return 'B'; break;
+	case 12: return 'C'; break;
+	case 13: return 'D'; break;
+	case 14: return 'E'; break;
+	case 15: return 'F'; break;
+	}
+	return '-1';
 }
 
 int convertToDecimal(string num, int base) {
@@ -79,11 +68,13 @@ int convertToDecimal(string num, int base) {
 
 string convertFromDecimal(int num, int newBase) {
     string result = "";
-
     while (num > 0) {
         int remainder = num % newBase;
-        char digitChar = (remainder < 10) ? ('0' + remainder) : ('A' + remainder - 10);
-        result = digitChar + result;
+		char digit = intTochar(remainder);
+		if (digit ==  '-1') {
+			return  "Ошибка: недопустимая цифра в числе.";
+		}
+        result = intTochar(remainder) + result;
         num /= newBase;
     }
 
