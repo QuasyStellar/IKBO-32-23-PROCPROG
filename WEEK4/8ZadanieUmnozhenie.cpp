b92f45e93c32f6065f5e7278b671a7c26a781e1f
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-10-13 21:51:38 +0300

    Update 8ZadanieUmnozhenie.cpp

--- WEEK4/8ZadanieUmnozhenie.cpp
+++ WEEK4/8ZadanieUmnozhenie.cpp
@@ -1,50 +1,83 @@
 #include <iostream>
-#include <list>
-#include <vector>
 using namespace std;
-float a11; float a22; float a33;
-float b11; float b22; float b33;
-float cena(int chi) {
-	switch (chi)
-	{ 
-	case 1: { return 1.2; break; }
-	case 2: { return 2.8; break; }
-	case 3: { return 5.0; break; }
-	case 4: { return 2.0; break; } 
-	} 
-}
-float comis(int chi) {
-	switch (chi)
-	{
-	case 1: { return 0.5; break; }
-	case 2: { return 0.4; break; }
-	case 3: { return 1.0; break; }
-	case 4: { return 1.5; break; }
-	}
-}
-int main() {
-	setlocale(LC_ALL, "ru-RU");
-	list <float> a1 = { 5, 2, 0, 10 }; list <float> a2 = { 3, 5, 2, 5 }; list <float> a3 = { 20, 0, 0, 0 };
-	int i = 0;
-	for (auto s : a1) { i++; a11 += cena(i) * s; } i = 0;
-	for (auto s : a2) { i++; a22 += cena(i) * s; } i = 0;
-	for (auto s : a3) { i++; a33 += cena(i) * s; } i = 0;
-	if (a11 > a22 and a22 > a33) { cout << "Первый продавец получил больше всегод денег с продажи\n"; }
-	else if (a22 > a11 and a11 > a33) { cout << "Второй продавец получил больше всегод денег с продажи\n"; }
-	else if (a33 > a22 and a22 > a11) { cout << "Третий продавец получил больше всегод денег с продажи\n"; }
-	if (a11 < a22 and a11 < a33) { cout << "Первый продавец получил меньше всего денег с продажи\n"; }
-	else if (a22 < a11 and a22 < a33) { cout << "Второй продавец получил меньше всего денег с продажи\n"; }
-	else if (a33 < a22 and a33 < a11) { cout << "Трейтий продавец получил меньше всего денег с продажи\n"; }
-	for (auto s : a1) { i++; b11 += cena(i) * s; } i = 0;
-	for (auto s : a2) { i++; b22 += cena(i) * s; } i = 0;
-	for (auto s : a3) { i++; b33 += cena(i) * s; } i = 0;
-	if (b11 > b22 and b11 > b33) { cout << "Первый продавец получил наибольшие комиссионные\n"; }
-	else if (b22 > b11 and b22 > b33) { cout << "Второй продавец получил наибольшие комиссионные\n"; }
-	else if (b33 > b22 and b33 > b11) { cout << "Третий продавец получил наибольшие комиссионные\n"; }
-	if (b11 < b22 and b11 < b33) { cout << "Первый продавец получил наименьшие комиссионные\n"; }
-	else if (b22 < b11 and b22 < b33) { cout << "Второй продавец получил наименьшие комиссионные\n"; }
-	else if (b33 < b22 and b33 < b11) { cout << "Третий продавец получил наименьшие комиссионные\n"; }
-	cout << "Общая сумма денег, вырученных за проданные товары: " << a11 + a22 + a33 << endl;
-	cout << "Всешо комиссионных: " << b11 + b22 + b33 << endl;
-	cout << "Всего прошло через продавцов: " << a11 + a22 + a33 + b11 + b22 + b33;
+
+int main()
+{
+    double A[3][4] = {
+        {5, 2, 0, 10},
+        {3, 5, 2, 5},
+        {20, 0, 0, 0} };
+    double B[4][2] = {
+        {1.2, 0.5},
+        {2.8, 0.4},
+        {5, 1},
+        {2, 1.5} };
+    double C[3][2] = {0};
+    int maxProd = 0;
+    int minProd = 0;
+    double maxMoney = 0;
+    double minMoney = 10000000;
+    double maxComss = 0;
+    double minComss = 10000000;
+    double sumMoney = 0;
+    double sumComss = 0;
+    double allMoney = 0;
+
+    for (int x = 0; x < 3; x++) { 
+        for (int y = 0; y < 2; y++) {
+            for (int i = 0; i < 4; i++) {
+                C[x][y] += A[x][i] * B[i][y];
+            }
+        }
+    }
+
+    for (int x = 0; x < 3; x++) { 
+        for (int y = 0; y < 2; y++) {
+            cout << C[x][y] << "         ";
+        }
+        cout << endl;
+    }
+
+    for (int x = 0; x < 3; x++) {
+        if (C[x][0] > maxMoney) {
+            maxProd = x + 1;
+            maxMoney = C[x][0];
+        }
+        if (C[x][0] < minMoney) {
+            minProd = x + 1;
+            minMoney = C[x][0];
+        }
+    }
+    cout << "Продавец " << maxProd << " выручил больше всего денег с продажи: " << maxMoney << endl;
+    cout << "Продавец " << minProd << " выручил меньше всего денег с продажи: " << minMoney << endl;
+
+    maxProd = 0;
+    minProd = 0;
+    for (int x = 0; x < 3; x++) {
+        if (C[x][1] > maxComss) {
+            maxProd = x + 1;
+            maxComss = C[x][1];
+        }
+        if (C[x][1] < minComss) {
+            minProd = x + 1;
+            minComss = C[x][1];
+        }
+    }
+    cout << "Продавец " << maxProd << " Получил наибольшие комиссионные " << maxComss << endl;
+    cout << "Продавец " << minProd << " Получил наименьшие комиссионные " << minComss << endl;
+
+    for (int x = 0; x < 3; x++) {
+        sumMoney += C[x][0];
+    }
+    cout << "Общая сумма: " << sumMoney << endl;
+
+    for (int x = 0; x < 3; x++) {
+        sumComss += C[x][1];
+    }
+    cout << "Сумма коммисионных: " << sumComss << endl;
+
+    for (int x = 0; x < 3; x++) {
+        allMoney += C[x][0] + C[x][1];
+    }
+    cout << "Общая сумма денег, прошедших через руки продавцов: " << allMoney << endl;
 }
