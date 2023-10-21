26dff7aa379c776eb6cc1b4df6398051787299bc
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-10-21 11:59:57 +0300

    Update 2ZadanieReshEras.cpp

--- HW5/2ZadanieReshEras.cpp
+++ HW5/2ZadanieReshEras.cpp
@@ -2,43 +2,36 @@
 
 using namespace std;
 
-void sieve_eratosthenes(int n) {
-    bool* is_prime = new bool[n + 1];
-    for (int i = 0; i <= n; i++) {
-        is_prime[i] = true;
-    }
-    is_prime[0] = is_prime[1] = false;
-
-    for (int p = 2; p * p <= n; p++) {
-        if (is_prime[p]) {
-            for (int i = p * p; i <= n; i += p) {
-                is_prime[i] = false;
-            }
-        }
-    }
-
-    cout << "Простые числа в диапазоне от 2 до " << n << ": ";
-    for (int i = 2; i <= n; i++) {
-        if (is_prime[i]) {
-            cout << i << " ";
-        }
-    }
-    cout << endl;
-
-    delete[] is_prime; // Освобождаем выделенную память
-}
-
 int main() {
+    setlocale(LC_ALL, "");
     int n;
     cout << "Введите натуральное число: ";
     cin >> n;
 
     if (n >= 2) {
-        sieve_eratosthenes(n);
+        bool* is_prime = new bool[n + 1];
+        for (int i = 0; i <= n; i++) {
+            is_prime[i] = true;
+        }
+        is_prime[0] = is_prime[1] = false;
+
+        for (int m = 2; m < n; m++) {
+            for (int j = m * 2; j < n; j += m) {
+                is_prime[j] = false;
+            }
+        }
+
+        cout << "Простые числа в диапазоне от 2 до " << n << ": ";
+        for (int i = 2; i < n; i++) {
+            if (is_prime[i]) {
+                cout << i << " ";
+            }
+        }
+        cout << endl;
+
+        delete[] is_prime;
     }
     else {
         cout << "Пожалуйста, введите число больше или равное 2." << endl;
     }
-
-    return 0;
 }
