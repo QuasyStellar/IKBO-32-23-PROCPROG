a34f978555ae55dd9487e7af05672f35007a12da
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-10-28 16:27:55 +0300

    Update ZadanieSharikiNoAlg.cpp

--- DOPOLNITELNO/ZadanieSharikiNoAlg.cpp
+++ DOPOLNITELNO/ZadanieSharikiNoAlg.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-// Функция для генерации перестановок
 int perestanovka(int arr[], int i, int n) {
     if (i == n) {
-        // Перестановка завершена, проверяем, совпадает ли номер с порядковым номером действия
         for (int j = 0; j < n; j++) {
             if (arr[j] == j + 1) {
                 return 1;
@@ -15,10 +13,8 @@ int perestanovka(int arr[], int i, int n) {
 
     int sum = 0;
     for (int m = i; m < n; m++) {
-        // Меняем местами элементы с номерами i и m
         swap(arr[i], arr[m]);
         sum += perestanovka(arr, i + 1, n);
-        // Возвращаем исходное состояние
         swap(arr[i], arr[m]);
     }
 
@@ -26,6 +22,7 @@ int perestanovka(int arr[], int i, int n) {
 }
 
 int main() {
+    setlocale(LC_ALL, "");
     int n;
     cout << "Введите количество шариков: ";
     cin >> n;
@@ -35,8 +32,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         balls[i] = i + 1;
     }
-
     int totalPermutations = perestanovka(balls, 0, n);
-
     cout << "Кол-во возможных перестановок: " << totalPermutations << endl;
 }
