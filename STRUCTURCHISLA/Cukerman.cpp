9f88810a8f8437453206b455604ba97dca892983
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-11-11 18:27:17 +0300

    Update Cukerman.cpp

--- STRUCTURCHISLA/Cukerman.cpp
+++ STRUCTURCHISLA/Cukerman.cpp
@@ -1,46 +1,39 @@
 #include <iostream>
-#include <vector>
 
-// Функция для вычисления числа Цукермана
-int cukermanNumber(int n, std::vector<int>& memo) {
+using namespace std;
+
+// Function to calculate the Cukerman number
+int cukermanNumber(int n) {
     if (n == 0) return 1;
-    
-    if (memo[n] != -1) {
-        return memo[n];
-    }
-    
+
     int result = 0;
     for (int k = 0; k < n; ++k) {
-        result += cukermanNumber(k, memo) * cukermanNumber(n - 1 - k, memo);
+        result += cukermanNumber(k) * cukermanNumber(n - 1 - k);
     }
-    
-    memo[n] = result;
+
     return result;
 }
 
-// Функция для вывода чисел Цукермана в заданном интервале
+// Function to print Cukerman numbers in the specified range
 void printCukermanNumbers(int start, int end) {
-    std::vector<int> memo(end + 1, -1);
-
-    std::cout << "Числа Цукермана в интервале [" << start << ", " << end << "]: ";
+    cout << "Cukerman numbers in the range [" << start << ", " << end << "]: ";
     for (int i = start; i <= end; ++i) {
-        std::cout << cukermanNumber(i, memo) << " ";
+        cout << cukermanNumber(i) << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 int main() {
     int start, end;
 
-    // Ввод интервала
-    std::cout << "Введите начальное значение интервала: ";
-    std::cin >> start;
-    std::cout << "Введите конечное значение интервала: ";
-    std::cin >> end;
+    // Input the range
+    cout << "Enter the starting value of the range: ";
+    cin >> start;
+    cout << "Enter the ending value of the range: ";
+    cin >> end;
 
-    // Вывод чисел Цукермана в заданном интервале
+    // Print Cukerman numbers in the specified range
     printCukermanNumbers(start, end);
 
-    retu
-      rn 0;
+    return 0;
 }
