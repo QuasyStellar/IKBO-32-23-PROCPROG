7b14e5c61c0f21327ac0e7ac84ab361367980bac
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-11-11 18:50:53 +0300

    Update Narayana.cpp

--- STRUCTURCHISLA/Narayana.cpp
+++ STRUCTURCHISLA/Narayana.cpp
@@ -1,21 +1,68 @@
-#include <iostream>
 
-int narayana(int n, int k) {
-    if (n == 0 && k == 0) {
-        return 1;
+// CPP program to find Narayana number N(n, k)
+#include<bits/stdc++.h>
+
+using namespace std;
+ 
+// Return product of coefficient terms in formula
+
+int productofCoefficient(int n, int k)
+{
+
+    int C[n + 1][k + 1];
+ 
+
+    // Calculate value of Binomial Coefficient
+
+    // in bottom up manner
+
+    for (int i = 0; i <= n; i++)
+
+    {
+
+        for (int j = 0; j <= min(i, k); j++)
+
+        {
+
+            // Base Cases
+
+            if (j == 0 || j == i)
+
+                C[i][j] = 1;
+ 
+
+            // Calculate value using previously
+
+            // stored values
+
+            else
+
+                C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+
+        }
+
     }
-    if (n == 0 || k == 0 || k == n + 1) {
-        return 0;
-    }
-    return narayana(n - 1, k - 1) + narayana(n - 1, k) + narayana(n - 1, k + 1);
+ 
+
+    return C[n][k] * C[n][k - 1];
 }
+ 
+// Returns Narayana number N(n, k)
 
-int main() {
-    int n, k;
-    std::cout << "Enter values for n and k: ";
-    std::cin >> n >> k;
+int findNN(int n, int k)
+{
 
-    std::cout << "Narayana(" << n << ", " << k << ") = " << narayana(n, k) << std::endl;
+    return (productofCoefficient(n, k)) / n;
+}
+ 
+// Driven Program
+
+int main()
+{
+
+    int n = 8, k = 5;
+
+    cout << findNN(n, k) << endl;
 
     return 0;
 }
