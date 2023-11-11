f10a46fe731e2351c31b351f0d700026ab91a3a5
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-11-11 18:57:08 +0300

    Update Narayana.cpp

--- STRUCTURCHISLA/Narayana.cpp
+++ STRUCTURCHISLA/Narayana.cpp
@@ -1,68 +1,31 @@
-
-// CPP program to find Narayana number N(n, k)
-#include<bits/stdc++.h>
+#include<iostream>
 
 using namespace std;
- 
-// Return product of coefficient terms in formula
-
-int productofCoefficient(int n, int k)
-{
-
+int findNN(int n, int k){
     int C[n + 1][k + 1];
- 
-
-    // Calculate value of Binomial Coefficient
-
-    // in bottom up manner
-
-    for (int i = 0; i <= n; i++)
-
-    {
-
-        for (int j = 0; j <= min(i, k); j++)
-
-        {
-
-            // Base Cases
-
-            if (j == 0 || j == i)
-
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= min(i, k); j++){
+            if (j == 0 || j == i){
                 C[i][j] = 1;
- 
-
-            // Calculate value using previously
-
-            // stored values
-
-            else
-
+                }
+            else{
                 C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
-
+                }
         }
-
     }
- 
-
-    return C[n][k] * C[n][k - 1];
+    return (C[n][k] * C[n][k - 1]) / n;
 }
- 
-// Returns Narayana number N(n, k)
 
-int findNN(int n, int k)
-{
 
-    return (productofCoefficient(n, k)) / n;
-}
- 
-// Driven Program
-
-int main()
-{
-
-    int n = 8, k = 5;
-
-    cout << findNN(n, k) << endl;
-
-    return 0;
+int main() {
+    setlocale(LC_ALL, "");
+    int n, m;
+    cout << "Введите начало и конец интервала: ";
+    cin >> m >> n;
+    for (int i = m; i<=n; i++){
+         for (int j = 0; j<=i; j++){
+            cout << findNN(i, j) << " ";
+        }
+        cout << endl;
+    }
 }
