acb910a656abcfcccd3001ab61e6a34ee9722341
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-11-11 18:42:57 +0300

    Update Cukerman.cpp

--- STRUCTURCHISLA/Cukerman.cpp
+++ STRUCTURCHISLA/Cukerman.cpp
@@ -1,42 +1,27 @@
 #include <iostream>
-
-// Функция для вычисления произведения цифр числа
-int productOfDigits(int num) {
+using namespace std;
+int cukerman(int num) {
     int product = 1;
-    while (num != 0) {
-        product *= num % 10;
-        num /= 10;
+    int temp = num;
+    while (temp != 0) {
+        product *= temp % 10;
+        temp /= 10;
     }
-    return product;
+    if (product == 0){return 0;}
+    return (num % product) == 0;
 }
 
-// Функция для проверки, является ли число Цукермана
-bool isCukermanNumber(int num) {
-    return (num % productOfDigits(num)) == 0;
-}
-
-// Функция для вывода чисел Цукермана в заданном интервале
-void printCukermanNumbers(int start, int end) {
-    std::cout << "Числа Цукермана в интервале [" << start << ", " << end << "]: ";
-    for (int i = start; i <= end; ++i) {
-        if (isCukermanNumber(i)) {
-            std::cout << i << " ";
-        }
-    }
-    std::cout << std::endl;
-}
 
 int main() {
-    int start, end;
-
-    // Ввод интервала
-    std::cout << "Введите начальное значение интервала: ";
-    std::cin >> start;
-    std::cout << "Введите конечное значение интервала: ";
-    std::cin >> end;
-
-    // Вывод чисел Цукермана в заданном интервале
-    printCukermanNumbers(start, end);
-
-    return 0;
+    setlocale(LC_ALL, "");
+    int m, n;
+    cout << "Введите началo и конец интервала: ";
+    cin >> m >> n;
+    cout << "Числа Цукермана в интервале " << m << ", " << n << ": ";
+    for (int i = m; i <= n; ++i) {
+        if (cukerman(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
 }
