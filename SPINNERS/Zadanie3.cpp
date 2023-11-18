a1bc1d459004cccf017cb9ef356caad0317c8866
QuasyStellar <76686085+QuasyStellar@users.noreply.github.com>
2023-11-18 11:02:03 +0300

    Update Zadanie3.cpp

--- SPINNERS/Zadanie3.cpp
+++ SPINNERS/Zadanie3.cpp
@@ -4,20 +4,19 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "");
-    int n, m;
+    int m, n;
     cout << "Введите значение m и n: ";
-    cin >> n >> m;
+    cin >> m >> n;
 
     int x = 0;
     int y = 0;
 
-    for (int x1 = 0; x1 < n; ++x1) {
+    for (int x1 = 0; x1 < n; x1++) {
         x += n - x1;
     }
-
-    for (int y1 = 0; y1 < m; ++y1) {
+    for (int y1 = 0; y1 < m; y1++) {
         y += m - y1;
     }
 
-    cout << "Результат: " << ax * y << endl;
+    cout << "Результат: " << x * y << endl;
 }
